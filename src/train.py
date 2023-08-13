9c7dee29f06a74c09a19154cf0eb8ea4515ea044
AmitRanjan235 <99864721+AmitRanjan235@users.noreply.github.com>
2023-08-13 16:14:55 +0530

    train updated

--- src/train.py
+++ src/train.py
@@ -16,9 +16,11 @@ def train_one_epoch(train_dataloader, model, optimizer, loss):
     if torch.cuda.is_available():
         # YOUR CODE HERE: transfer the model to the GPU
         # HINT: use .cuda()
+        model=model.cuda()
+
 
     # YOUR CODE HERE: set the model to training mode
-    
+    model.train()
     train_loss = 0.0
 
     for batch_idx, (data, target) in tqdm(
@@ -34,14 +36,17 @@ def train_one_epoch(train_dataloader, model, optimizer, loss):
 
         # 1. clear the gradients of all optimized variables
         # YOUR CODE HERE:
+        optimizer.zero_grad()
         # 2. forward pass: compute predicted outputs by passing inputs to the model
-        output  = # YOUR CODE HERE
+        output  = model(data)# YOUR CODE HERE
         # 3. calculate the loss
-        loss_value  = # YOUR CODE HERE
+        loss_value  = loss(output,target)# YOUR CODE HERE
         # 4. backward pass: compute gradient of the loss with respect to model parameters
         # YOUR CODE HERE:
+        loss_value.backword()
         # 5. perform a single optimization step (parameter update)
         # YOUR CODE HERE:
+        optimizer.step()
 
         # update average training loss
         train_loss = train_loss + (
@@ -60,7 +65,7 @@ def valid_one_epoch(valid_dataloader, model, loss):
 
         # set the model to evaluation mode
         # YOUR CODE HERE
-
+        model.eval()
         if torch.cuda.is_available():
             model.cuda()
 
@@ -77,9 +82,9 @@ def valid_one_epoch(valid_dataloader, model, loss):
                 data, target = data.cuda(), target.cuda()
 
             # 1. forward pass: compute predicted outputs by passing inputs to the model
-            output  = # YOUR CODE HERE
+            output  =model(data) # YOUR CODE HERE
             # 2. calculate the loss
-            loss_value  = # YOUR CODE HERE
+            loss_value  =loss(output,target) # YOUR CODE HERE
 
             # Calculate average validation loss
             valid_loss = valid_loss + (
@@ -104,7 +109,7 @@ def optimize(data_loaders, model, optimizer, loss, n_epochs, save_path, interact
     # plateau
     # HINT: look here: 
     # https://pytorch.org/docs/stable/optim.html#how-to-adjust-learning-rate
-    scheduler  = # YOUR CODE HERE
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True) # YOUR CODE HERE
 
     for epoch in range(1, n_epochs + 1):
 
@@ -129,12 +134,12 @@ def optimize(data_loaders, model, optimizer, loss, n_epochs, save_path, interact
 
             # Save the weights to save_path
             # YOUR CODE HERE
-
+            torch.save(model.state_dict(), save_path)
             valid_loss_min = valid_loss
 
         # Update learning rate, i.e., make a step in the learning rate scheduler
         # YOUR CODE HERE
-
+        scheduler.step(valid_loss)
         # Log the losses and the current learning rate
         if interactive_tracking:
             logs["loss"] = train_loss
@@ -156,6 +161,7 @@ def one_epoch_test(test_dataloader, model, loss):
 
         # set the model to evaluation mode
         # YOUR CODE HERE
+        model.eval()
 
         if torch.cuda.is_available():
             model = model.cuda()
@@ -172,16 +178,16 @@ def one_epoch_test(test_dataloader, model, loss):
                 data, target = data.cuda(), target.cuda()
 
             # 1. forward pass: compute predicted outputs by passing inputs to the model
-            logits  = # YOUR CODE HERE
+            logits  =model(data) # YOUR CODE HERE
             # 2. calculate the loss
-            loss_value  = # YOUR CODE HERE
+            loss_value  =loss(logits,target) # YOUR CODE HERE
 
             # update average test loss
             test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss_value.data.item() - test_loss))
 
             # convert logits to predicted class
             # HINT: the predicted class is the index of the max of the logits
-            pred  = # YOUR CODE HERE
+            pred  = logits.argmax(dim=1) # YOUR CODE HERE
 
             # compare predictions to true label
             correct += torch.sum(torch.squeeze(pred.eq(target.data.view_as(pred))).cpu())
